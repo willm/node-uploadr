d9c30d5f783fb242a137dd50b38e198e9eeb6255
Will Munn <willmunn@hotmail.com>
2012-04-17 22:44:38 +0100

    modified formidable example to post progress

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-(function  (){
+/*(function  (){
 	var express = require('express'),
-		path = require('path');
+		formidable = require('formidable');
 	
 	server = express.createServer();
 	
-	server.configure(function configureAppAndMiddleware() {
+	server.configure(function() {
 		server.set('view engine', 'jade');
-		server.use(express.bodyParser());
+		server.use(express.bodyParser({uploadDir:'./uploads'}));
+		var form = new formidable.IncomingForm();
+		form.onPart = function(part) {
+		  part.addListener('data', function() {
+			console.log('boo');
+		  });
+		}
 	});
 	
 	server.get('/', function  (req, res){
@@ -14,11 +20,69 @@
 	});
 	
 	server.post('/upload', function (req, res){
-		
-		console.log(req.body);
-		res.send(200);
+		console.log("req");
 	});
 	
 	server.listen(8080);
 	console.log("server listening on port 8080");
-})()
+})()*/
+TEST_PORT = 8080;
+TEST_TMP = './uploads'
+var http = require('http'),
+    util = require('util'),
+    formidable = require('formidable'),
+    server;
+
+server = http.createServer(function(req, res) {
+  if (req.url == '/') {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+      '<form action="/upload" enctype="multipart/form-data" method="post">'+
+      '<input type="text" name="title"><br>'+
+      '<input type="file" name="upload" multiple="multiple"><br>'+
+      '<input type="submit" value="Upload">'+
+      '</form>'
+    );
+  } else if (req.url == '/upload') {
+    var form = new formidable.IncomingForm(),
+        files = [],
+        fields = [];
+
+    form.uploadDir = TEST_TMP;
+
+	form.onPart = function(part) {
+	  part.addListener('data', function() {
+		console.log(percentageComplete(form.bytesExpected,form.bytesReceived));
+		form.handlePart(part);
+	  });
+	}
+	
+	function percentageComplete (total, received){
+		return percent = received * 100 / total;
+	}
+
+    form
+      .on('field', function(field, value) {
+        console.log(field, value);
+        fields.push([field, value]);
+      })
+      .on('file', function(field, file) {
+        console.log(field, file.size);
+        files.push([field, file]);
+      })
+      .on('end', function() {
+        console.log('-> upload done');
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.write('received fields:\n\n '+util.inspect(fields));
+        res.write('\n\n');
+        res.end('received files:\n\n '+util.inspect(files));
+      });
+    form.parse(req);
+  } else {
+    res.writeHead(404, {'content-type': 'text/plain'});
+    res.end('404');
+  }
+});
+server.listen(TEST_PORT);
+
+console.log('listening on http://localhost:'+TEST_PORT+'/');
